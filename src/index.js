0653d92086a1375d3cc9f29223a70896c15508bb
Taras Liuzniak <lyuznyaktaras@gmail.com>
2020-11-07 21:09:41 +0200

    interim save

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ function setStart(event) {
 
 function setStop(event) {
     clearInterval(timerId);
-    // startButton.addEventListener('click', setStart);
+    startButton.addEventListener('click', setStart);
 }
 
 
